ea1a7858979e919ae8cfb29d988e763bd4894b7b
Paul Toussaint <ptoussaint@ace-si.com>
2023-10-26 14:53:17 +0200

    first commit

---
+++ src/app/models/client.ts
@@ -0,0 +1,14 @@
+export class Client {
+    nom: string;
+    prenom: string;
+    adresse: string;
+    cp: string;
+    ville: string;
+    tel: string;
+    email: string;
+    civilite: string;
+    password: string;
+    login: string;
+    pays: string;
+  }
+  
